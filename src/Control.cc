ba85ae74be66487d22e77faa3cf25bdb1e091f77
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-11 09:50:19 -0400

    Create skeleton of launch function

--- src/Control.cc
+++ src/Control.cc
@@ -4,3 +4,21 @@
 #include <string>
 
 using namespace std;
+
+void Control::launch() {
+  int choice, owner;
+
+  while (1) {
+    view.selectMenu(owner);
+  }
+
+  if (owner == 0) return;
+
+  while (1) {
+    if (owner == 1) {
+      view.showCustomerMenu(choice);
+    } else {
+      view.showOwnerMenu(choice);
+    }
+  }
+}
