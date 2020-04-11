502bf8b672cce3973091950d3bdd218c7429382d
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-11 16:22:43 -0400

    Updat constructor

--- src/Control.cc
+++ src/Control.cc
@@ -1,11 +1,9 @@
 #include "../include/Control.h"
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-Control::Control() noexcept : db("3005 project", "stephen", "supersecret", "127.0.0.1", "5432") {}
 Control::Control() noexcept
     : db("3005 project", "stephen", "supersecret", "127.0.0.1", "5432") {}
 
