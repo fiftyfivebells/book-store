c20085f0fe6fd81752e64158785371291e16a73a
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-11 10:50:48 -0400

    Create basic functionality for Database class

---
+++ src/Database.cc
@@ -0,0 +1,23 @@
+#include "../include/Database.h"
+#include <iostream>
+
+Database::Database(string &db, string &user, string &pass, string &addr,
+                   string &port)
+    : conn("dbname = " + db + " user = " + user + " password = " + pass +
+           " hostaddr = " + addr + " port = " + port) {
+  // string c = "dbname = " + db + " user = " + user + " password = " + pass + "
+  // hostaddr = " + addr + " port = " + port;
+  if (conn.is_open()) {
+    std::cout << "Connected successfully" << std::endl;
+  }
+}
+
+Database::~Database() {
+  conn.disconnect();
+}
+
+result Database::executeQuery(string &query) {
+  nontransaction n(conn);
+
+  return result(n.exec(query));
+}
