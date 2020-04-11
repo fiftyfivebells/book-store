11f81775032cfa718d990720d98b4f9fae6d8d76
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-11 09:21:10 -0400

    Create View.h header file

---
+++ include/View.h
@@ -0,0 +1,16 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <string>
+
+using namespace std;
+
+class View {
+ public:
+  void showCustomerMenu(int&);
+  void showOwnerMenu(int&);
+  void readInt(int&);
+  void readStr(string&);
+};
+
+#endif
