0bd2399891b2099561b025cbfbdcd299c53996fd
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-10 20:40:25 -0400

    Add queries to create dummy publishers

---
+++ SQL/publisher-queries.sql
@@ -0,0 +1,6 @@
+insert into publisher (email_address, street_name, street_number, apt_number, city, publisher_state, zip, phone_number, bank_account) values
+('sales@dutton.com', 'Elm Street', '6', 'D', 'South Deerfield', 'Massachusetts', '01046', '800 123 1234', '0982346773'),
+ ('sales@placeholder.com', 'Willow Street', '1776', '', 'Austin', 'Texas', '83674', '800 987 4321', '1238758475'),
+ ('sales@mcsweeney.com', 'Summit Avenue', '55', '', 'Portland', 'Oregon', '64738', '800 647 5287', '9283746574'),
+ ('sales@theseoldbooks.com', 'Memorial Avenue', '1900', '15', 'Cambridge', 'Massachusetts', '01928', '613 999 7382', '7486574663'),
+ ('sales@everyman.com', 'Electric Avenue', '1', '', 'Pasadena', 'California', '84121', '407 111 7564', '3427584775');
