017f89966066994fa54ed2e6c31a8f5bfab54ec3
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-12 16:39:09 -0400

    Run the auto-formatter

--- include/View.h
+++ include/View.h
@@ -1,26 +1,27 @@
 #ifndef VIEW_H
 #define VIEW_H
 
-#include "Cart.h"
 #include <string>
+#include "Cart.h"
 
 using namespace std;
 
 class View {
  public:
-  void selectMenu(int&);
-  void showCustomerMenu(int&);
-  void showOwnerMenu(int&);
-  void readInt(int&);
-  void readStr(string&);
-  void readLine(string&);
+  void selectMenu(int &);
+  void showCustomerMenu(int &);
+  void showOwnerMenu(int &);
+  void readInt(int &);
+  void readStr(string &);
+  void readLine(string &);
   void printStr(string);
   void clearScreen();
-  void bookSearchMenu(int&);
-  void afterSearch(int&);
-  void showCart(Cart*);
+  void bookSearchMenu(int &);
+  void afterSearch(int &);
+  void showCart(Cart *);
   void displayBook(CartItem *);
-  void displayCartOps(int&);
+  void displayCartOps(int &);
+  void displayLogInOrRegister(int &, int &);
 };
 
 #endif
