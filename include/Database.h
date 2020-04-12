fcffd3ab7cf8e888c8cd9b403d737297418f247c
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-11 23:57:01 -0400

    Reformat the & in the function arguments

--- include/Database.h
+++ include/Database.h
@@ -16,11 +16,11 @@ class Database {
  public:
   Database(string, string, string, string, string);
   ~Database();
-  result executeQuery(string&);
-  result queryBookAuthor(string&);
-  result queryBookTitle(string&);
-  result queryBookPublisher(string&);
-  result queryBookGenre(string&);
+  result executeQuery(string &);
+  result queryBookAuthor(string &);
+  result queryBookTitle(string &);
+  result queryBookPublisher(string &);
+  result queryBookGenre(string &);
 
  private:
   connection conn;
