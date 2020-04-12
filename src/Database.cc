6df5984613a1e95f8b544a15b074d738d0e04cd9
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-12 16:25:40 -0400

    Create addAddress function

--- src/Database.cc
+++ src/Database.cc
@@ -145,4 +145,19 @@ void Database::addCustomer(Customer *c, Address *ship, Address *bill) {
 
 }
 
+void Database::addAddress(string email, string type, Address *a) {
+  nontransaction n(conn);
+  string query = "insert into customer_address (address_type, customer_email, street_number, street_name, apt_number, city, state, zip) values ";
+
+  query += "'(" + type + "', ";
+  query += email + "', ";
+  query += a->getStreetNum() + ", '";
+  query += a->getStreetName() + "', '";
+  query += a->getAptNum() + "', '";
+  query += a->getCity() + "', '";
+  query += a->getState() + "', ";
+  query += a->getZip() + ";";
+
+  result(n.exec(query));
+  n.abort();
 }
