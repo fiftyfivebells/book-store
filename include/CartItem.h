cb61b2c4cb8d244ac3f64a31998d26000f7250b1
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-11 20:13:28 -0400

    Update constructor and add getters for CartItem

--- include/CartItem.h
+++ include/CartItem.h
@@ -1,16 +1,24 @@
 #ifndef CART_ITEM_H
 #define CART_ITEM_H
 
+#include <deque>
 #include <string>
 
 using namespace std;
 
 class CartItem {
  public:
-  CartItem(string="", int=0);
+  CartItem(string = "", string = "", int = 0);
+  void addAuthor(string);
+  string getIsbn();
+  string getTitle();
+  deque<string> getAuthors();
+  int getQuantity();
 
  private:
   string isbn;
+  string title;
+  deque<string> authors;
   int quantity;
 };
 
--- src/CartItem.cc
+++ src/CartItem.cc
@@ -1,6 +1,16 @@
 #include "../include/CartItem.h"
 
-CartItem::CartItem(string isbn, int quantity)
-    : isbn(isbn), quantity(quantity) {}
+CartItem::CartItem(string isbn, string title, int quantity)
+    : isbn(isbn), quantity(quantity), title(title) {}
 
+void CartItem::addAuthor(string author) {
+  authors.push_back(author);
+}
 
+string CartItem::getIsbn() { return isbn; }
+
+string CartItem::getTitle() { return title; }
+
+int CartItem::getQuantity() { return quantity; }
+
+deque<string> CartItem::getAuthors() { return authors; }
