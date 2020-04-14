0d32a5c68ebbf4a4fdb64c902679ee2a70f552c8
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-14 11:00:19 -0400

    Create a function to order from the publisher and update books

--- SQL/procedures.sql
+++ SQL/procedures.sql
@@ -63,3 +63,11 @@ create or replace function expense_report(start_date date, end_date date)
 $$
 language sql stable;
 
+create or replace function order_from_publisher(ownerid uuid, email varchar, isbn varchar, quant numeric)
+  returns void as
+  $$
+  insert into orders_books (owner_id, email_address, isbn, quantity) values
+    (ownerid, email, isbn, quant);
+  update book set quantity = quantity + quant where isbn = isbn;
+  $$
+language sql volatile;
