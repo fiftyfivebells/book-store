1434fb51aab5403f49232346d3a3aae24d5954eb
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-14 11:15:21 -0400

    Handle DB setup in main and pass to Control as argument

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,10 @@
 #include "../include/Control.h"
+#include "../include/Database.h"
 
 int main() {
-  Control control;
+  Database *db = new Database();
+  db->setConnection("3005 project", "stephen", "supersecret", "127.0.0.1", "5432");
+  Control control(db);
   control.launch();
+  delete db;
 }
