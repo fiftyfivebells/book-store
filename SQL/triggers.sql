25d295f4ea737d9f72422f76f055c16562058c05
fiftyfivebells <stephen.l.bell@gmail.com>
2020-04-14 21:11:17 -0400

    Create trigger to update quantity when books drop too low

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -2,3 +2,8 @@ create trigger update_publishes
     after insert on book
     for each row
     execute procedure insert_into_publishes();
+
+create trigger restock_books
+    after update of quantity on book
+    for each row
+    execute procedure replenish_book_stock();
